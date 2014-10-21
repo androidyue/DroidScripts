bd4bdcf2604969c9be3bcf9383eb711495c8d1ee
Andrew Wallace <andrewallanwallace@gmail.com>
2014-10-21 13:39:29 +0800

    Add except branches

--- ruby/removeMergedBranches.rb
+++ ruby/removeMergedBranches.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+exceptBranches = ['*', 'master', 'pre']
 for branch in `cd #{ARGV[0]} && git branch -l`.split(' ')
-    next if branch == '*'
+    next if exceptBranches.include? branch
     system("git branch -d #{branch}")
 end
