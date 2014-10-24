43d75fa7e4296e8850ea97a0e9f4dbe63f405b44
Andrew Wallace <andrewallanwallace@gmail.com>
2014-10-24 10:04:38 +0800

    Make script better understanding

--- ruby/removeMergedBranches.rb
+++ ruby/removeMergedBranches.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-exceptBranches = ['*', 'master', 'pre']
-for branch in `cd #{ARGV[0]} && git branch -l`.split(' ')
+exceptBranches = ['master', 'pre', 'develop']
+for branch in `cd #{ARGV[0]} && git branch -l`.split(' ') - ['*']
     next if exceptBranches.include? branch
     system("git branch -d #{branch}")
 end
