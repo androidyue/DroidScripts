0fcb5f6a5c5da5e40ec78a8c7cb7db4c20359af9
Andrew Wallace <andrewallanwallace@gmail.com>
2014-11-28 19:38:21 +0800

    Rework less variable

--- ruby/removeOldFiles.rb
+++ ruby/removeOldFiles.rb
@@ -6,6 +6,5 @@ daysBefore= ARGV[1]
 pattern = (ARGV.size == 3) ? ARGV[2]: '*'
 edenTime = Time.now.to_i - daysBefore.to_i * 86400
 Dir[destDir + '/' + pattern ].each{|child|
-    lastModifiedTime = File.mtime(child).to_i
-    system "rm -rfv #{child}"   if (lastModifiedTime < edenTime)
+    system "rm -rfv #{child}"   if (File.mtime(child).to_i < edenTime)
 }
