7f22701be4a05d1ec4dd48b041d1a31c603bfe02
Andrew Wallace <andrewallanwallace@gmail.com>
2014-11-29 10:57:11 +0800

    escape glob and support remove files which has space in the file name

--- ruby/removeOldFiles.rb
+++ ruby/removeOldFiles.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-#Usage: ruby removeOldFiles.rb dest_dir day_before [pattern]
-destDir= ARGV[0]
+#Usage: ruby removeOldFiles.rb "dest_file_pattern" days_before
+destFilePattern= ARGV[0]
 daysBefore= ARGV[1]
-pattern = (ARGV.size == 3) ? ARGV[2]: '*'
 edenTime = Time.now.to_i - daysBefore.to_i * 86400
-Dir[destDir + '/' + pattern ].each{|child|
+Dir[destFilePattern].each{|child|
     system "rm -rfv #{child}"   if (File.mtime(child).to_i < edenTime)
 }
