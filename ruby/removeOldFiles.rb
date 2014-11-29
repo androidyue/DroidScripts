58bf3dc8a0d826cf3b467e3eed2efc8f1ff1c17b
Andrew Wallace <andrewallanwallace@gmail.com>
2014-11-29 17:32:45 +0800

    modified:   removeOldFiles.rb

--- ruby/removeOldFiles.rb
+++ ruby/removeOldFiles.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-#Usage: ruby removeOldFiles.rb "dest_file_pattern" days_before
+#Usage: ruby removeOldFiles.rb "dest_file_pattern" days_ago
 destFilePattern= ARGV[0]
-daysBefore= ARGV[1]
-edenTime = Time.now.to_i - daysBefore.to_i * 86400
+daysAgo= ARGV[1]
+edenTime = Time.now.to_i - daysAgo.to_i * 86400
 Dir[destFilePattern].each{|child|
     system "rm -rfv #{child}"   if (File.mtime(child).to_i < edenTime)
 }
